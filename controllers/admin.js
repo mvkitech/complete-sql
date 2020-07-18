3303ebc75416d74bb07d49a6ebe10f0d51975c9e
mvkitech <info@mvkitech.com>
2020-07-18 16:42:39 -0600

    Check in following end of the Sequelization code changes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,23 @@
 const Product = require('../models/product');
 
+exports.getProducts = (req, res, next) => {
+  req.user
+    .getProducts()
+    .then((products) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
   });
 };
 
@@ -13,9 +26,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
+    .then((result) => {
+      console.log('Product Created');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -24,17 +48,21 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  });
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then((products) => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -43,29 +71,30 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
-};
-
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then((result) => {
+      console.log('Product Updated');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log('Product Deleted');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
 };
--- readme.txt
+++ readme.txt
@@ -12,3 +12,36 @@ this respository as well as the forked repository.
 As usual with anything related to Node JS, the "node_modules" folder was excluded so the 
 need to run "npm install" will have to be done if/when this repository is copied to a 
 different machine and/or folder than the original master instance.
+
+Deviations from course's original code:
+
+1) The "util/database.js" file was excluded from the repository since it contains the
+   password to the local MySQL database. A copy of the original "database.js" file is
+   included in this repository called "util/databaseOriginal.js" and one only needs to
+   rename this file to "database.js" and of course use the appropriate DB password.
+
+2) A number of minor tweaks were made to the code such as re-ordering some of the 
+   functions in the Shop and Admin controllers, minor tweaks to some of the console
+   log statements.
+
+What's coming next?
+
+1) This is the end of the SQL section of the Udemy course. Moving forward Max is now
+   going to be working with the MongoDB. I suspect the fundamentals of the models and
+   controllers should not go through radical changes though clearly there will be some
+   changes since Max will be dealing with Mongoose and not Sequelize. 
+
+2) The code will be forked, so this branch will remain as a SQL solution. But now it 
+   is time for myself to get to work as I do not like how "app.js" is organized. there
+   is extemely tight coupling there towards setting up Sequelize and I am sure there
+   is a better solution to de-couple the code to make it cleaner. So expect changes
+   outside the scope of Max's course to happen.
+
+3) I do suspect I will also need to revisit this branch to merge non-database changes 
+   such as User Authentication, E-Mail Support, Validation, Error Handling as well as 
+   File Upload/Download and Pagination. 
+
+4) Finally I would like to revisit the UI views screens (likely at the end of the 
+   course) as while the styling isn't too bad, the layout of all the screens are
+   pretty basic and could definitely be improved.
+   
